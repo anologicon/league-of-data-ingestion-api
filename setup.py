5a9906a63c5b1aa857ae93372e5ef9cd6d7a40fb
anologicon <pauloraimundi2009@gmail.com>
2023-03-11 22:37:54 -0300

    feat: moved to package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='league_of_data_ingestion_api',
     version='0.0.1',
     zip_safe=False,
     python_requires=">=3.6",
