99c75f8a427ea7ec3ad79e28c4020576da3715c0
anologicon <pauloraimundi2009@gmail.com>
2023-03-11 21:09:51 -0300

    hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# League of data ingestion API
+
+This is my package to abstract league of legends API calls
+
+It's for simple use.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='toolbox',
+    version='0.0.1',
+    author='Paulo Raimundi',
+    author_email='pauloraimundi2009@gmail.com',
+    description='A league of legends API tools',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mike-huls/toolbox',
+    project_urls = {
+        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    },
+    license='MIT',
+    packages=['toolbox'],
+    install_requires=['requests'],
+)
