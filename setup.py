e52b6facb3ec1161575fc7e95346e3ad806d221a
anologicon <pauloraimundi2009@gmail.com>
2023-03-11 22:44:14 -0300

    feat: change package name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='league_of_data_ingestion_api',
+    name='leagueapi',
     version='0.0.2',
     zip_safe=False,
     python_requires=">=3.6",
