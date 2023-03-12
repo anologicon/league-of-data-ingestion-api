7d030bb0322127c04c40bdc4e2e53008a8d6a52b
anologicon <pauloraimundi2009@gmail.com>
2023-03-11 23:36:26 -0300

    feat: change packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='leagueapi',
-    version='0.0.5',
+    version='0.0.6',
     zip_safe=False,
     python_requires=">=3.6",
     description='A league of legends API tools',
