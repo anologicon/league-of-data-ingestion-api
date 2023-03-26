90f48d081b443a67355196b4958726bf692c7772
anologicon <pauloraimundi2009@gmail.com>
2023-03-25 21:07:29 -0300

    version 13

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='league_api',
-    version='0.0.12',
+    version='0.0.13',
     zip_safe=False,
     python_requires=">=3.6",
     description='A league of legends API tools',
