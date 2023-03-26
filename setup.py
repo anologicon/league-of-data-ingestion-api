c1a4003e7e87711a17c7518747ae95cd3c4b5d9f
anologicon <pauloraimundi2009@gmail.com>
2023-03-25 21:45:06 -0300

    new versions

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='league_api',
-    version='0.0.16',
+    version='0.0.17',
     zip_safe=False,
     python_requires=">=3.6",
     description='A league of legends API tools',
     license='MIT',
-    packages=['league-api'],
+    packages=['league_api'],
     install_requires=[
         'requests',
         'tqdm',
